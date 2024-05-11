5ed0033ae16bf5eafb2883279b1d75ababf43f7a
keithchasen <keithchasen89@gmail.com>
2024-05-11 21:08:57 +0200

    basic canvas set up

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,7 +1,36 @@
+import { useEffect, useRef } from "react";
+
 function Game() {
+  let canvasRef = useRef(null);
+
+  useEffect(() => {
+    let cnv = canvasRef?.current;
+    let ctx = cnv?.getContext('2d');
+
+    cnv.width = window.innerWidth;
+    cnv.height = window.innerHeight;
+
+    const animate = () => {
+        requestAnimationFrame(animate);
+    
+        if (ctx && cnv) {
+            ctx.clearRect(0, 0, cnv.width, cnv.height);
+        }
+    }
+
+    animate();
+
+    window.addEventListener('resize', () => {
+        cnv.width = window.innerWidth;
+        cnv.height = window.innerHeight;
+
+        console.log({ w: cnv.width, h: cnv.height })
+    })
+  })
+
   return (
     <>
-      GAME
+      <canvas ref={canvasRef}></canvas>
     </>
   )
 }
